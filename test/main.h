98328186c6e9af7e8df866779a8a19bbf3366bad
Josué Makuta <makutajosue@gmail.com>
2023-04-25 16:09:45 +0200

    Feat : fix bugs in filing

--- test/main.h
+++ test/main.h
@@ -1,43 +1,66 @@
 #ifndef MAIN_H
 #define MAIN_H
 
-#include <unistd.h>
+
 #include <stdio.h>
 #include <stdlib.h>
 #include <stdarg.h>
+#include <unistd.h>
 
 /**
-* struct converter - defines a structure for specifiers and functions
+* struct printer_convert - a structure for specifiers and functions in the project
 *
-* @spec: The specifier
-* @func: The function associated
+* @spec: A variable for the specifier
+* @func: A variable for a function
 */
-typedef struct converter
+typedef struct printer_converter
 {
 	char *spec;
 	int (*func)(va_list);
 } conv_type;
 
-int put_character(char c);
-char *_memcpy(char *dest, char *src, unsigned int n);
-char *rev_str(char *s);
-void write_str(char *str);
-unsigned int number_len(unsigned int num, int base);
-int print_reverse_string(va_list arg);
-int rotter(va_list list);
-int print_digit_number(va_list args);
-int print_unsigned_number(unsigned int n);
-int print_percentage(__attribute__((unused))va_list list);
-int hexadecimal_checker(int num, char x);
-int print_bin(va_list list);
-int print_octal_number(va_list list);
-int print_hexadecimal_lower(va_list list);
-int print_hexadecimal_upper(va_list list);
-int print_integer(va_list list);
-int print_unsigned(va_list list);
-int print_string(va_list list);
-int print_character(va_list list);
+/** The prototypes */
+
 int print(const char *format, conv_type conv_list[], va_list arg_list);
+
 int _printf(const char *format, ...);
+
+int print_unsigned_number(unsigned int n);
+
+int print_percentage(__attribute__((unused))va_list list);
+
+int hexadecimal_checker(int num, char x);
+
+int print_bin(va_list list);
+
+int print_octal_number(va_list list);
+
+int print_hexadecimal_lower(va_list list);
+
+int print_hexadecimal_upper(va_list list);
+
+int put_character(char c);
+
+char *_memcpy(char *dest, char *src, unsigned int n);
+
+char *rev_str(char *s);
+
+void write_str(char *str);
+
+unsigned int number_len(unsigned int num, int base);
+
+int print_reverse_string(va_list arg);
+
+int rotter(va_list list);
+
+int print_digit_number(va_list args);
+
+int print_integer(va_list list);
+
+int print_unsigned(va_list list);
+
+int print_string(va_list list);
+
+int print_character(va_list list);
 
 #endif
